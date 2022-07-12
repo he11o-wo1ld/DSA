55bd65516a542820deb27a7a0796958974f47d8a
krishdp <95845806+krishdp@users.noreply.github.com>
2022-07-12 12:13:29 +0530

    Update Queston.

---
+++ Interview Test/Almabase/findDuplicateProfile.py
@@ -0,0 +1,6 @@
+profile1 = { 'id': 1, 'email': 'knowkanhai@gmail.com', 'first_name': 'Kanhai', 'last_name': 'Shah', 'class_year': 2012, 'date_of_birth': '1990-10-11', }
+profile2 = { id: 2, email: 'knowkanhai@gmail.com', first_nam
+e: ‘Kanhai1’, last_name: ‘Shah’, class_year: 2012, date_of_bir
+th: ’1990-10-11’}
+
+print(profile1)
